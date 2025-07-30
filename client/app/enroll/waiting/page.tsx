4174a03ab0040c0cae4aa8fc8d1a914ed0e0d89f
moiseiyaa <moiseiyaa@gmail.com>
2025-07-30 17:20:42 +0200

    now

--- client/app/enroll/waiting/page.tsx
+++ client/app/enroll/waiting/page.tsx
@@ -1,17 +1,9 @@
-// Server-side wrapper for client-only dialog
 export const dynamic = 'force-dynamic';
 
-import { Suspense } from 'react';
-import dynamicImport from 'next/dynamic';
-
-const WaitingClient = dynamicImport(() => import('./WaitingClient'), { ssr: false });
+import WaitingClient from './WaitingClient';
 
 export default function WaitingPage() {
-  return (
-    <Suspense>
-      <WaitingClient />
-    </Suspense>
-  );
+  return <WaitingClient />;
 }
 
 
